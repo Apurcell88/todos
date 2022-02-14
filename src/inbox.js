a23544572e359d0f5badb65a3f0a12e4c14e7818
Adam Purcell <apurcell88@gmail.com>
2022-02-14 16:10:09 -0600

    Created a factory function that so far adds todos, but not working as intended yet. Modified HTML to have more icons in the nav.

--- src/inbox.js
+++ src/inbox.js
@@ -1,9 +1,31 @@
 import addTaskBtn from "./index.js";
-const taskPopup = document.querySelector('#add-task-popup');
 
-function loadInboxPopup() {
+let todos = [];
+// const title = document.querySelector('#todo-to-add-text');
+
+export function loadInboxPopup() {
+    const taskPopup = document.querySelector('#add-task-popup');
     addTaskBtn.classList.add('hide-display');
     taskPopup.classList.add('show-display-flex');
 }
 
-export default loadInboxPopup;
+// Factory function in charge of inbox todos including creation
+export function inboxTodos() {
+    const title = document.querySelector('#todo-to-add-text');
+    const description = document.querySelector('[data-description]');
+    const priority = document.querySelector('[data-priority]');
+
+    return {
+        createTodo () {
+            todos.push({
+                title: title.value,
+                description: description.value,
+                priority: priority.value,
+                // id: '',
+            });
+            return todos;
+        }
+    }
+}
+
+export default todos;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-import loadInboxPopup from './inbox.js';
+import { loadInboxPopup, inboxTodos, todos } from './inbox.js';
 
 const addTaskBtn = document.querySelector('#add-task-btn');
+const addTodoBtn = document.querySelector('#add-btn');
 
 addTaskBtn.addEventListener('click', (e) => {
     loadInboxPopup();
 });
+
+addTodoBtn.addEventListener('click', (e) => {
+    const todo = inboxTodos();
+    todo.createTodo();
+    console.log(todo);
+    // console.log(todos);
+})
 
 export default addTaskBtn;
