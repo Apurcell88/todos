a9dd9d368aa9738d46f2ef91e14bc4c3a1512c7d
Adam Purcell <apurcell88@gmail.com>
2022-02-17 15:42:31 -0600

    Date input added but not styled. Minor CSS refactor.

--- src/inboxDOM.js
+++ src/inboxDOM.js
@@ -6,8 +6,12 @@ export function displayTodo(todos) {
 
         const completedTodoCheckbox = document.createElement('input');
         completedTodoCheckbox.setAttribute('type', 'checkbox');
-        completedTodoCheckbox.classList.add('margin-right');
+        completedTodoCheckbox.classList.add('margin-right', 'font-size');
         individualTodoContainer.appendChild(completedTodoCheckbox);
+
+        const dueDate = document.createElement('input');
+        dueDate.setAttribute('type', 'date');
+        individualTodoContainer.appendChild(dueDate);
 
         const todoEl = document.createElement('span');
         todoEl.textContent = `${todo.title} Priority: ${todo.priority}`;
